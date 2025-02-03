e099bde23f78970166e024cf97f9b1519c1ad27c
Shakhrukh Abdugapar <69681238+shahruhabd@users.noreply.github.com>
2025-02-03 13:51:20 +0500

    Update settings.py

--- prepify/settings.py
+++ prepify/settings.py
@@ -80,16 +80,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'prepify.urls'
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+    "http://157.245.123.144:8085",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000", 
-#     "http://157.245.123.144:8085",
-# ]
-
-CORS_ALLOW_METHODS = ["GET", "OPTIONS", "PATCH", "POST", "PUT"]
-CORS_ALLOW_HEADERS = ["*"]
-
 
 TEMPLATES = [
     {
